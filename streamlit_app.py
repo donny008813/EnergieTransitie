5d1de91daba019f03d45cef163e4477a06aa51dd
donny008813 <167076782+donny008813@users.noreply.github.com>
2024-10-18 08:53:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,4 @@ st.title("Energy Consumption Dashboard")
 st.markdown('''Op deze dashboard zal worden getoond hoe de energie behoefte van een industrieterrein is bepaald. Hiervoor is de beschikbare data van Dutch Fresh Port
 gebruikt. Met deze informatie is er eerst een beeld gemaakt specifiek voor dit terrein. Na verder onderzoek zal het mogelijk zijn om van een ander terrein gegevens
 in te vullen en hiervan het energieverbruik te kunnen tonen over de toekomst. Hierin is vooral gekijken naar het verbruik van vrachtwagens en het verbruik
-van de gebouwen per verschillend type bedrijf. '''
+van de gebouwen per verschillend type bedrijf. ''')
